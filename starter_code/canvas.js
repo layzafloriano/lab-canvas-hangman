c4a734fa89c981541512c3d5c5319e55911750e2
Layza Floriano <layzafloriano@gmail.com>
2019-06-20 21:15:28 -0300

    commit

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,38 @@
+class HangmanCanvas {
+  constructor(secretWord) {
+    this.secretWord = secretWord;
+    this.ctx = document.getElementById("hangman").getContext("2d");
+  }
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+  createBoard() {
+    this.ctx.clearRect(0,0,850,1250);
+  }
+
+  drawLines(pi) {
+    let x1 = 400;
+    let x2 = 450;
+    let y = 700;
+    for (let i = 0; i < pi.length; i += 1) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(x1, y);
+      this.ctx.lineTo(x2, y);
+      this.ctx.stroke();
+      x1 = x1 + 60;
+      x2 = x1 + 50;
+    }
+  }
+
+  writeCorrectLetter(index) {
+    this.ctx.fillText(this.secretWord[index], 400 , 700);
+  }
+
+  writeWrongLetter(letter, errorsLeft) {}
+
+  drawHangman(shape) {}
+
+  gameOver() {}
+
+  winner() {}
 }
 
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
+const test = new HangmanCanvas("secret");
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,79 @@
 var hangman;
 
-// function Hangman() {
+class Hangman {
+  constructor(){
+    this.words = ['teste', 'oi', 'teste 2'];
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+    this.canvas = new HangmanCanvas("teste");
+  }
 
-// }
+  //gera uma palavra aleatória e retorna ela
+   getWord(){
+     let random = Math.floor(Math.random() * this.words.length) - 1;
+     return this.words[random]
+   }
 
-// Hangman.prototype.getWord = function () {
+   // Verifica se é letra
+   checkIfLetter(keyCode) {
+     if((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)) {
+       return true;
+     }
+     return false;
+   }
 
-// };
+   //Verifica e ganhou
+   checkWinner(){
+     let winner = this.secretWord.toUpperCase().split('').sort().join('');
+     let letters = this.guessedLetter.toUpperCase().split('').sort().join('');
+     if(winner===letters){
+       return true;
+     }
+     return false;
+   }
+   // Vê se a letra já foi clicada
+   checkClickedLetters(key) {
+     console.log(this.letters.includes(key));
+      if(this.letters.includes(key)) {
+        return false
+      }
+      this.letters.push(key);
+      for(let i = 0; i < this.secretWord.length; i += 1){
+        if(this.secretWord[i] === key) {
+          this.addCorrectLetter(i);
+        }
+        else {
+          this.addWrongLetter(key);
+        }
+      }
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+    
 
-// };
+      return true
+    }
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+    //adiciona letra  certa (recebe index)
+    addCorrectLetter(letter) {
+    this.guessedLetter += this.secretWord[letter].toUpperCase();
+    this.canvas.writeCorrectLetter(letter);
+    }
 
-// };
+    // subtrai a "vida"
+    addWrongLetter(letter) {
+     this.errorsLeft -= 1; 
+     this.canvas.writeWrongLetter(letter, this.errorsLeft);
+    }
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+    // Verifica game over
+    checkGameOver(){
+      if(this.errorsLeft <= 0){
+        return true
+      }
+      return false
+    }
+}
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
@@ -38,5 +81,9 @@ document.getElementById('start-game-button').onclick = function () {
 
 
 document.onkeydown = function (e) {
-
+  if(e && hangman.checkIfLetter(e.keyCode)) {
+    console.log(e.keyCode, "LETRA");
+  } else {
+    console.log(e.keyCode, "NAO É LETRA");
+  }
 };
